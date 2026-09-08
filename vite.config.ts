9d5b8c8cd86f1c0cfe438bf4ac00da78206ae863
Natthaphong48110 <nuttaphong967@gmail.com>
2026-09-08 09:43:23 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/game-test/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
